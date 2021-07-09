a4e7765a714c3b518ae380764597807f3defdb3c
MarpMarp236 <marpthelegend@gmail.com>
2021-07-09 22:43:52 +0200

    Exported more files in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports.welcome = require('./functions/welcome');
+module.exports.memberCount = require('./functions/membercount');
 module.exports.Moderation = require('./classes/moderation');
 module.exports.Cooldown = require('./classes/Cooldown');
