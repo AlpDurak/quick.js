07005c7e7c03df277d8e2541c82a0762093ff6a8
Maddax Lallatin <65627323+papamaddax@users.noreply.github.com>
2021-07-10 08:53:23 -0400

    Start logging system, adding join logs

---
+++ src/classes/logs.js
@@ -0,0 +1,19 @@
+class Logs {
+    async join(Discord, client, member, channelID) {
+        if (!Discord) throw new TypeError("\"Discord\" is not defined")
+        if (!client) throw new TypeError("\"client\" is not defined")
+        if (!member) throw new TypeError("\"member\" is not defined")
+        if (!channelID) throw new TypeError("\"channelID\" is not defined")
+        const channel = member.guild.channels.cache.get(channelID)
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(member.username, member.displayAvatarURL({
+                dynamic: true
+            }))
+            .setThumbnail(member.guild.iconURL())
+            .setTimestamp()
+            .setDescription(`${member.username} has joined the server`)
+            .setColor("RANDOM")
+        channel.send(embed)
+    } // End Join
+
+}
